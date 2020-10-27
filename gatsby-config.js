96e314a8253531a166d2b36d046e36823ba40c94
lizlove <elizabethlovero@gmail.com>
2020-10-27 13:40:01 -0400

    Insert Gatsby default starter and basic Kate info.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,34 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Kate Lovero`,
+    description: `Dr. Kate Lovero is Assistant Professor in the Department of Psychiatry at Columbia University Vagelos College of Physicians and Surgeons.`,
+    author: `@katelovero`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Dr. Kate Lovero is Assistant Professor in the Department of Psychiatry at Columbia University Vagelos College of Physicians and Surgeons.  Her research uses a community-based participatory approach to identify best practice for implementing evidence-based treatments for adolescent mental illness in low-resource settings. Currently, she is collaborating with the Ministry of Health in Mozambique to evaluate strategies for implementing adolescent mental health services within primary care facilities.</p>
+    <p>Prior to her position at Columbia, Dr. Lovero was a NIH Global Health Equity Scholar and Fulbright Postdoctoral Fellow in Brazil, studying barriers to care and developing methods to improve services for maternal-child health in partnership with local health officials and clinicians.</p>
+    <p>Dr. Lovero earned her PhD in Neuroscience from the University of California, San Francisco, where she characterized molecular structures underlying healthy and pathological brain development.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/publications/">Publications</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
