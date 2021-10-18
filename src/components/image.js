04641885220543cd1c4daeee1df963066b5eaa4b
lizlove <elizabethlovero@gmail.com>
2021-10-18 15:45:08 -0400

    New headshot.

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "lovero-headshot.png" }) {
+      placeholderImage: file(relativePath: { eq: "lovero-headshot-2.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
